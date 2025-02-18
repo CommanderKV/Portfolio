e4832199c5ef16e174c50bff2388e3bc11056631
CommanderKV <kylervisser@gmail.com>
2025-02-18 01:22:14 -0500

    Implement responsive navigation toggle; update CSS for dropdown and enhance layout for mobile devices

--- Server/static/js/base.js
+++ Server/static/js/base.js
@@ -1,16 +1,4 @@
-// Grab the dropdown elements
-const dropdown = document.querySelector(".dropdown");
-const toggle = document.querySelector(".dropdown-toggle");
-const caret = document.querySelector(".caret");
 
-toggle.addEventListener("click", function (event) {
-    event.preventDefault();
-    dropdown.classList.toggle("active");
-});
-
-// Close dropdown if clicked outside
-document.addEventListener("click", function (event) {
-    if (!dropdown.contains(event.target)) {
-        dropdown.classList.remove("active");
-    }
+document.getElementById("nav-toggle").addEventListener("click", function() {
+    document.getElementById("head-container").classList.toggle("dropdown");
 });
