e1d2b693bf18e777aee2c096c8a31dd474ed91ae
CommanderKV <kylervisser@gmail.com>
2025-02-21 02:30:10 -0500

    Enhance project card interaction: open project URL in a new tab for "See project" button and navigate to custom URL for other clicks; add last updated and archived status to project details

--- Server/static/js/projects.js
+++ Server/static/js/projects.js
@@ -1,8 +1,14 @@
-
 const projectCards = document.querySelectorAll('.project-card');
 
 projectCards.forEach(card => {
-    card.addEventListener("click", () => {
-        window.location.href = "/projects/" + card.getAttribute("data");
+    card.addEventListener("click", (event) => {
+        const target = event.target;
+        if (target.closest('.project-cta a')) {
+            // If the "See project" button is clicked, go to the project URL
+            window.open(target.closest('.project-cta a').href, '_blank');
+        } else {
+            // If anything else in the project card is clicked, go to the custom URL
+            window.location.href = "/projects/" + card.getAttribute("data");
+        }
     });
 });
