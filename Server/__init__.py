032135246a2099ef34feb7f85e9f3b17d2776b70
CommanderKV <kylervisser@gmail.com>
2025-02-24 12:32:31 -0500

    Refactor geoLocation handling: move to app initialization

--- Server/__init__.py
+++ Server/__init__.py
@@ -2,7 +2,7 @@ from logging import FileHandler, WARNING
 from flask_sqlalchemy import SQLAlchemy
 from .getColors import run as getColors
 from datetime import timedelta
-from flask import Flask, render_template
+from flask import *
 import logfire
 import sys
 import os
@@ -86,10 +86,62 @@ def create_app():
     # ----------------------------------
     from . import main
     from .controllers import login, dashboard
+    from .tools import getGeoData
     app.register_blueprint(main.app)
     app.register_blueprint(login.app)
     app.register_blueprint(dashboard.app)
     
+    # Geo location data by ip when the post request is made
+    @app.before_request
+    def geoLocation():
+        if request.environ.get("HTTP_X_FORWARDED_FOR"):
+            ip = request.environ.get("HTTP_X_FORWARDED_FOR")
+        elif request.remote_addr:
+            ip = request.remote_addr
+        else:
+            ip = None
+            logfire.warn("Could not determine the IP address of the request")
+
+        with logfire.span(f"Request from IP(s): {ip}", request=request.__dict__):
+            if "geoData" in session.keys():
+                logfire.debug("Using cached geo data", geoData=session.get("geoData"))
+            else:
+                if ip is not None:
+                    if "," in ip:
+                        data = []
+                        for ip in ip.split(", ", 10):
+                            # check if its more than an ip address
+                            # 123.123.123.123 = 15 characters
+                            if len(data) >= 15:
+                                logfire.warn("Too many IP addresses in the request", ip=ip)
+                                break
+
+                            logfire.debug(f"Getting geo data for IP: {ip}")
+                            geoData = getGeoData(ip)
+                            
+                            if geoData["status"] == "fail":
+                                logfire.warn("Failed to get geo data", geoData=geoData)
+                                continue
+                            
+                            data.append(geoData)
+                        
+                        logfire.debug("Saving geo data to session")
+                        session["geoData"] = data
+                    
+                    else:
+                        logfire.debug(f"Getting geo data for IP: {ip}")
+                        geoData = getGeoData(ip)
+                        
+                        if geoData["status"] == "fail":
+                            logfire.warn("Failed to get geo data", geoData=geoData)
+                            return
+                        
+                        logfire.debug("Saving geo data to session")
+                        session["geoData"] = geoData
+                    
+                    logfire.info("Obtained geo data", data=session.get("geoData"))
+
+
     @app.errorhandler(404)
     def not_found_error(error):
         return render_template("base.html", disableHeader=True, disableContact=True), 404
--- Server/main.py
+++ Server/main.py
@@ -1,66 +1,12 @@
-from .controllers import login
 from .tools import *
 from flask import *
-import regex as re
 import logfire
-import time
 
 # Make the app
 app = Blueprint("main", __name__)
 
 # Start the scheduler
 startBackgroundScheduler()
-
-# Geo location data by ip when the post request is made
-@app.before_request
-def geoLocation():
-    if request.environ.get("HTTP_X_FORWARDED_FOR"):
-        ip = request.environ.get("HTTP_X_FORWARDED_FOR")
-    elif request.remote_addr:
-        ip = request.remote_addr
-    else:
-        ip = None
-        logfire.warn("Could not determine the IP address of the request")
-
-    with logfire.span(f"Request from IP(s): {ip}", request=request.__dict__):
-        if "geoData" in session.keys():
-            logfire.debug("Using cached geo data", geoData=session.get("geoData"))
-        else:
-            if ip is not None:
-                if "," in ip:
-                    data = []
-                    for ip in ip.split(", ", 10):
-                        # check if its more than an ip address
-                        # 123.123.123.123 = 15 characters
-                        if len(data) >= 15:
-                            logfire.warn("Too many IP addresses in the request", ip=ip)
-                            break
-
-                        logfire.debug(f"Getting geo data for IP: {ip}")
-                        geoData = getGeoData(ip)
-                        
-                        if geoData["status"] == "fail":
-                            logfire.warn("Failed to get geo data", geoData=geoData)
-                            continue
-                        
-                        data.append(geoData)
-                    
-                    logfire.debug("Saving geo data to session")
-                    session["geoData"] = data
-                
-                else:
-                    logfire.debug(f"Getting geo data for IP: {ip}")
-                    geoData = getGeoData(ip)
-                    
-                    if geoData["status"] == "fail":
-                        logfire.warn("Failed to get geo data", geoData=geoData)
-                        return
-                    
-                    logfire.debug("Saving geo data to session")
-                    session["geoData"] = geoData
-                
-                logfire.info("Obtained geo data", data=session.get("geoData"))
-
 
 # GET: /
 @app.route("/", methods=["GET"])
